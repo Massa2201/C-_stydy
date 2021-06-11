cafd631fc385dafbba9bc0a1c8ef7fedbff37531
Imai Masashi <chrono.trigger.2424@gmail.com>
2021-06-11 21:48:58 +0900

    C++復習

---
+++ atcoder_ex/ex10.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  int i = 0;
+  int j = 0;
+  cin >> A >> B;
+
+cout << "A:";
+  while(i < A) {
+    cout << "]";
+    i++;
+  }
+  cout << "" << endl;
+    cout << "B:";
+
+  while(j < B) {
+    cout << "]";
+    j++;
+  }
+  cout << "" << endl;
+
+}
