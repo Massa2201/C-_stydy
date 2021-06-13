93d639a8468982b016cc1294658d56ffae08a86a
Imai Masashi <chrono.trigger.2424@gmail.com>
2021-06-13 19:08:37 +0900

    実行時エラー

--- atcoder_ex/ex11.cpp
+++ atcoder_ex/ex11.cpp
@@ -3,34 +3,44 @@ using namespace std;
 
 int main() {
   int N, A;
+  int i = 1;
   cin >> N >> A;
 
   for(N; N > 0; N--) {
     
-    int i = 1;
     int b;
     string a;
 
-    cout << "符号を入力してください:";
+    //cout << "符号を入力してください:";
     cin >> a;
 
-    cout << "数字を入力してください";
+    //cout << "数字を入力してください";
     cin >> b;
 
     if(a == "+") {
-    cout << i + ":" + A + b << endl;
+      A += b;
+      cout << i << ":" << A << endl;
     }
 
-    if(a == "-") {
-    cout << i + ":" + A - b << endl;
+    else if(a == "-") {
+      A -= b;
+      cout << i << ":" << A << endl;
     }
 
-    if(a == "*") {
-    cout << i + ":" + A * b << endl;
+    else if(a == "*") {
+      A *= b;
+      cout << i << ":" <<  A << endl;
     }
 
-    if(a == "/") {
-    cout << i + ":" + A / b << endl;
+    else if(a == "/") {
+      A /= b;
+      cout << i << ":" << A << endl;
     }
+
+    else {
+      cout << "error!" << endl;
+    }
+
+    i++;
   }
 }
