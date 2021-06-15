1e3eff40437177fc34fbee9cdd6872afd52bc3d8
Imai Masashi <chrono.trigger.2424@gmail.com>
2021-06-16 04:09:58 +0900

    AC : a ~ o の母音以外はconsonantを出力

---
+++ atcoder_A/iaiwo-aoie.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  char c;
+  string v = "vowel";
+  string co = "consonant";
+  cin >> c;
+
+  if(c == 'a') {
+    cout << v << "\n";
+  }
+  else if(c == 'i') {
+    cout << v << "\n";
+  }
+
+  else if(c == 'u') {
+    cout << v << "\n";
+  }
+
+  else if(c == 'e') {
+    cout << v << "\n";
+  }
+
+  else if(c == 'o') {
+    cout << v << "\n";
+  }
+
+  else {
+    cout << co << "\n";
+  }
+}
