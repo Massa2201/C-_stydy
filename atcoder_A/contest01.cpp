01e5ca7cae7363652c064dc016ff0e6aeffe561f
Imai Masashi <chrono.trigger.2424@gmail.com>
2021-06-17 21:18:11 +0900

    AC : >> や <<を一行で書くとグループごとに分かれて獲得できる

--- atcoder_A/contest01.cpp
+++ atcoder_A/contest01.cpp
@@ -7,20 +7,9 @@ https://atcoder.jp/contests/abc048/tasks/abc048_a
 */
 int main() {
 
-  string S;
+  string A, s, C;
   string answer;
-  cin >> S;
+  cin >> A >> s >> C;
 
-  for(int i = 0; i < S.size(); i++) {
-    char c = S.at(i);
-  
-    if(S.at(i) == ' ' ) {
-      i++;
-      for(; S.at(i) == ' '; i++) {
-        
-        answer += S.at(i);
-      }
-    }
-  }
-    cout << answer << "\n";
+    cout << A[0] << s[0] << C[0] << "\n";
 }
