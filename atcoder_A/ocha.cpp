9c8fc20bc023deac22b34c2c24576386f74043a6
Imai Masashi <chrono.trigger.2424@gmail.com>
2021-06-19 22:54:03 +0900

    AC : 文字列の最後をどう指定するかがポイント

--- atcoder_A/ocha.cpp
+++ atcoder_A/ocha.cpp
@@ -4,21 +4,11 @@ using namespace std;
 int main() {
 
   string s;
-  int count = 0;
   cin >> s;
 
-  for(int i = 50; i > 0; i--) {
-    
-    if(i - 1 == null) {
-      if(s.at(i) == 'T') {
-        count = 1;
-      }
-    }
-  }
-  
-  if(count == 1) {
+  if(s.at(s.size() - 1) == 'T') {
     cout << "YES" << "\n";
-  }
+  } 
   else {
     cout << "NO" << "\n";
   }
