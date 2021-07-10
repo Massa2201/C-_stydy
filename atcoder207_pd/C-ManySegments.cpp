a1670513aec023ce39ad452620760eff292b687f
Imai Masashi <chrono.trigger.2424@gmail.com>
2021-07-10 20:34:13 +0900

    AC : 解答を参照

--- atcoder207_pd/C-ManySegments.cpp
+++ atcoder207_pd/C-ManySegments.cpp
@@ -6,4 +6,25 @@ int main() {
   int n;
   cin >> n;
   vector<int> l(n), r(n);
+  for (int i = 0; i < n; i++) {
+    int t;
+    cin >> t >> l[i] >> r[i];
+    l[i] *= 2;
+    r[i] *= 2;
+    if (t >= 3) {
+      l[i]++;
+    }
+    if (t % 2 == 0) r[i]--;
+  }
+
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      if (r[i] < l[j]) continue;
+      if (r[j] < l[i]) continue;
+      ++ans;
+    }
+  }
+
+  cout << ans << "\n";
 }
