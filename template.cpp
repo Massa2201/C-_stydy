8506ea40adf060e8675256d47fa02bbaf51aa3e0
Imai Masashi <chrono.trigger.2424@gmail.com>
2021-07-19 04:39:59 +0900

    2次元配列の書き方を追加

--- template.cpp
+++ template.cpp
@@ -6,5 +6,13 @@ int main() {
   
 }
 
-// 2次元配列 縦横 3 × 4
-// vector<vector<int>> data(3, vector<int>(4));
+
+/*
+
+2次元配列 縦横 3 × 4
+vector<vector<int>> data(3, vector<int>(4));
+
+2次元配列 N x N 中に'-'を入れている
+vector<vector<char>> table(N, vector<char>(N, '-'));
+
+*/
