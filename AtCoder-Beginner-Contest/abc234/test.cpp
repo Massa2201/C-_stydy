83cdb0f2763b7b8317dd509aee885657b10470ee
Imai Masashi <chrono.trigger.2424@gmail.com>
2022-01-08 23:06:09 +0900

    デバック環境のテスト

---
+++ AtCoder-Beginner-Contest/abc234/test.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<math.h>
+#include<iostream>
+using namespace std;
+using ll = long long;
+
+int main() {
+  ll k;
+  cin >> k;
+
+  
+
+}
+
+
+/*
+
+2次元配列 縦横 3 × 4
+vector<vector<int>> data(3, vector<int>(4));
+
+vector dp(n + 1, vector<int>(9));
+
+2次元配列 N x N 中に'-'を入れている
+vector<vector<char>> table(N, vector<char>(N, '-'));
+
+vector<int> vec(N);
+for (int i = 0; i < N; i++) {
+  cin >> vec.at(i);
+}
+*/
