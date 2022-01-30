c0cab054551f6589a68426566324a8fff11167b8
Imai Masashi <chrono.trigger.2424@gmail.com>
2022-01-30 20:56:32 +0900

    add

--- template.cpp
+++ template.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using ld = long double;
 
 int main() {
   
@@ -11,6 +12,10 @@ int main() {
 
 2次元配列 縦横 3 × 4
 vector<vector<int>> data(3, vector<int>(4));
+
+vector dp(n + 1, vector<int>(9));
+
+vector<int> vec(N);
 
 2次元配列 N x N 中に'-'を入れている
 vector<vector<char>> table(N, vector<char>(N, '-'));
