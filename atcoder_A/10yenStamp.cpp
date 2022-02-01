dc1aef6d751aa46621c225d8854249fe9a8f2bef
Imai Masashi <chrono.trigger.2424@gmail.com>
2022-02-01 14:03:15 +0900

    AC

---
+++ atcoder_A/10yenStamp.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int main() {
+  int x, y;
+  int a = 0;
+  int b = 0;
+  cin >> x >> y;
+  a = y - x;
+
+  if (a > 0) {
+    b = (a + 10 - 1) / 10;
+    cout << b << endl;
+  }
+  else {
+    cout << 0 << endl;
+  }
+
+}
+
+
+/*
+
+2次元配列 縦横 3 × 4
+vector<vector<int>> data(3, vector<int>(4));
+
+vector dp(n + 1, vector<int>(9));
+
+vector<int> vec(N);
+
+2次元配列 N x N 中に'-'を入れている
+vector<vector<char>> table(N, vector<char>(N, '-'));
+
+*/
