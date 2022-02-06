578a6a31dca9ebf86db670bea73fc112aff43278
Imai Masashi <chrono.trigger.2424@gmail.com>
2022-02-06 19:05:49 +0900

    AC: 累乗と絶対値

---
+++ AtCoder-Beginner-Contest/abc238/a.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int main() {
+  ld n, a, b;
+  ll cnt = 0;
+  ll ans = 0;
+
+  cin >> n;
+
+  a = pow(2, n);
+  a = fabs(a);
+  b = n * n;
+
+  if (a > b) {
+    cout << "Yes" << endl;
+  }
+  else {
+    cout << "No" << endl;
+  }
+}
+
+
+/*
+
+2次元配列 縦横 3 × 4
+vector<vector<int>> data(3, vector<int>(4));
+
+vector dp(n + 1, vector<int>(9));
+
+vector<int> vec(N);
+
+2次元配列 N x N 中に'-'を入れている
+vector<vector<char>> table(N, vector<char>(N, '-'));
+
+*/
